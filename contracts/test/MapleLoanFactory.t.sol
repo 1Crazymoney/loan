d1433c7fae960721ed9ff474edad80342e78d409
Michael De Luca <michael.deluca@circle-free.com>
2021-09-17 19:16:16 -0400

    feat: Proxied MapleLoan

--- contracts/test/MapleLoanFactory.t.sol
+++ contracts/test/MapleLoanFactory.t.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.7;
 
 import { DSTest }               from "../../modules/ds-test/src/test.sol";
-import { MockImplementationV1 } from "../../modules/proxy-factory/contracts/test/mocks/Mocks.sol";
 
-import { MapleLoanFactory } from "../MapleLoanFactory.sol";
+import { MapleLoan }            from "../MapleLoan.sol";
+import { MapleLoanFactory }     from "../MapleLoanFactory.sol";
+import { MapleLoanInitializer } from "../MapleLoanInitializer.sol";
 
 import { Borrower } from "./accounts/Borrower.sol";
 import { Governor } from "./accounts/Governor.sol";
@@ -22,136 +23,236 @@ contract MapleGlobalsMock {
 contract MapleLoanFactoryTest is DSTest {
 
     Borrower             borrower;
+    Borrower             notBorrower;
     Governor             governor;
     Governor             notGovernor;
     MapleGlobalsMock     globals;
-    MapleLoanFactory     loanFactory;
-    MockImplementationV1 implementationV1;
+    MapleLoanFactory     factory;
+    MapleLoan            mapleLoanV1;
+    MapleLoan            mapleLoanV2;
+    MapleLoanInitializer initializerV1;
+    MapleLoanInitializer initializerV2;
 
+    function setUp() external {
+        borrower      = new Borrower();
+        governor      = new Governor();
+        initializerV1 = new MapleLoanInitializer();
+        initializerV2 = new MapleLoanInitializer();
+        mapleLoanV1   = new MapleLoan();
+        mapleLoanV2   = new MapleLoan();
+        notBorrower   = new Borrower();
+        notGovernor   = new Governor();
 
-    address implementationAddress1 = address(111);
-    address implementationAddress2 = address(333);
-    address initializer1           = address(222);
-    address initializer2           = address(444);
-    address migrator               = address(999);
-
-    bytes dummyArguments = bytes("Dummy arguments");
-
-    function setUp() public {
-        borrower         = new Borrower();
-        governor         = new Governor();
-        notGovernor      = new Governor();
-        globals          = new MapleGlobalsMock(address(governor));
-        loanFactory      = new MapleLoanFactory(address(globals));
-        implementationV1 = new MockImplementationV1();
+        globals = new MapleGlobalsMock(address(governor));
+        factory = new MapleLoanFactory(address(globals));
     }
 
-    function test_registerImplementation() public {
+    function test_registerImplementation() external {
         assertTrue(
-            !notGovernor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 1, implementationAddress1, initializer1),
-            "Should fail to register: Invalid governor"
+            !notGovernor.try_mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1)),
+            "Should fail to register if not governor"
         );
 
         assertTrue(
-            !governor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 0, implementationAddress1, initializer1),
-            "Should fail to register: Invalid version"
+            !governor.try_mapleLoanFactory_registerImplementation(address(factory), 0, address(mapleLoanV1), address(initializerV1)),
+            "Should fail to register an invalid version"
         );
 
         assertTrue(
-            !governor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 1, address(0), initializer1),
-            "Should fail to register: Invalid implementation address"
+            !governor.try_mapleLoanFactory_registerImplementation(address(factory), 1, address(0), address(initializerV1)),
+            "Should fail to register an invalid implementation address"
         );
 
         assertTrue(
-            governor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 1, implementationAddress1, initializer1),
-            "Should not fail to register"
+            governor.try_mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1)),
+            "Governor should be able to register"
         );
 
-        // Try to register again with the same version.
         assertTrue(
-            !governor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 1, implementationAddress2, address(0)),
-            "Should fail to register: Version already registered"
+            !governor.try_mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1)),
+            "Should fail to register an already registered version"
         );
-        
-        assertEq(loanFactory.latestVersion(),                   1,                      "Incorrect state of latestVersion");
-        assertEq(loanFactory.implementationOf(1),               implementationAddress1, "Incorrect state of implementationOf");
-        assertEq(loanFactory.versionOf(implementationAddress1), 1,                      "Incorrect state of versionOf");
-        assertEq(loanFactory.migratorForPath(1, 1),             initializer1,           "Incorrect state of migratorForPath");
+
+        assertEq(factory.implementationOf(1),             address(mapleLoanV1),   "Incorrect state of implementationOf");
+        assertEq(factory.versionOf(address(mapleLoanV1)), 1,                      "Incorrect state of versionOf");
+        assertEq(factory.migratorForPath(1, 1),           address(initializerV1), "Incorrect state of migratorForPath");
     }
 
-    function test_createLoan() public {
+    function test_setDefaultVersion() external {
+        governor.mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1));
+
         assertTrue(
-            !borrower.try_mapleLoanFactory_createLoan(address(loanFactory), dummyArguments),
-            "Should fail to create loan: Version is not registered yet"
+            !notGovernor.try_mapleLoanFactory_setDefaultVersion(address(factory), 1),
+            "Should fail to set default version if not governor"
         );
 
         assertTrue(
-            governor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 1, address(implementationV1), address(0)),
-            "Should not fail to register"
+            !governor.try_mapleLoanFactory_setDefaultVersion(address(factory), 2),
+            "Should fail to set default version if not registered"
         );
 
-        assertTrue(borrower.try_mapleLoanFactory_createLoan(address(loanFactory), dummyArguments), "Should not fail to create loan");
+        assertTrue(
+            governor.try_mapleLoanFactory_setDefaultVersion(address(factory), 1),
+            "Should be able to set default version"
+        );
 
-        assertEq(loanFactory.loanCount(), 1);
-        address loanAddress = loanFactory.loans(loanFactory.loanCount());
+        assertEq(factory.defaultVersion(), 1, "Incorrect state of defaultVersion");
 
-        // Create another loan with different arguments.
-        assertTrue(borrower.try_mapleLoanFactory_createLoan(address(loanFactory), "Dummy arguments 123"), "Should not fail to create loan");
-        assertEq(loanFactory.loanCount(), 2);
-        address newLoanAddress = loanFactory.loans(loanFactory.loanCount());
+        assertTrue(
+            !notGovernor.try_mapleLoanFactory_setDefaultVersion(address(factory), 0),
+            "Should fail to unset default version if not governor"
+        );
 
-        assertTrue(loanAddress != newLoanAddress, "Same address gets generated");
+        assertTrue(
+            governor.try_mapleLoanFactory_setDefaultVersion(address(factory), 0),
+            "Should be able to unset default version"
+        );
+
+        assertEq(factory.defaultVersion(), 0, "Incorrect state of defaultVersion");
     }
 
-    function test_enableUpgradePath() public {
-        assertTrue(
-            !notGovernor.try_mapleLoanFactory_enableUpgradePath(address(loanFactory), 1, 2, migrator),
-            "Should fail to upgrade path: Unauthorized"
-        );
+    function test_createLoan() external {
+        address[2] memory assets = [address(4567), address(9876)];
+
+        uint256[6] memory parameters = [
+            uint256(0),
+            uint256(10 days),
+            uint256(120_000),
+            uint256(100_000),
+            uint256(365 days / 6),
+            uint256(6)
+        ];
+
+        uint256[2] memory requests = [uint256(300_000), uint256(1_000_000)];
+
+        bytes memory arguments = initializerV1.encodeArguments(address(borrower), assets, parameters, requests);
 
         assertTrue(
-            !governor.try_mapleLoanFactory_enableUpgradePath(address(loanFactory), 1, 0, migrator),
-            "Should fail to upgrade path: Invalid version"
+            !borrower.try_mapleLoanFactory_createLoan(address(factory), arguments),
+            "Should fail to create loan for an unregistered version"
         );
 
-        assertTrue(
-            !governor.try_mapleLoanFactory_enableUpgradePath(address(loanFactory), 2, 1, migrator),
-            "Should fail to upgrade path: Invalid version"
-        );
+        governor.mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1));
+        governor.mapleLoanFactory_setDefaultVersion(address(factory), 1);
 
         assertTrue(
-            governor.try_mapleLoanFactory_registerImplementation(address(loanFactory), 1, implementationAddress1, initializer1),
-            "Should not fail to register"
+            !borrower.try_mapleLoanFactory_createLoan(address(factory), new bytes(0)),
+            "Should fail to create loan with invalid arguments"
         );
 
-        assertTrue(
-            governor.try_mapleLoanFactory_enableUpgradePath(address(loanFactory), 1, 2, migrator),
-            "Should not fail to upgrade path"
-        );
-        
-        assertEq(
-            loanFactory.migratorForPath(1, 2), migrator,
-            "Incorrect state of migratorForPath"
-        );
+        assertTrue(borrower.try_mapleLoanFactory_createLoan(address(factory), arguments), "Should not fail to create a loan");
+
+        assertEq(factory.loanCount(), 1);
+
+        assertTrue(borrower.try_mapleLoanFactory_createLoan(address(factory), arguments), "Should not fail to create a loan");
+
+        assertEq(factory.loanCount(), 2);
+
+        assertTrue(factory.loanAtIndex(0) != factory.loanAtIndex(1), "Loans should have unique addresses");
+
+        MapleLoan loan = MapleLoan(factory.loanAtIndex(0));
+
+        assertEq(loan.factory(),                           address(factory));
+        assertEq(loan.implementation(),                    address(mapleLoanV1));
+        assertEq(factory.versionOf(loan.implementation()), 1);
     }
 
-    function test_disableUpgradePath() public {
+    function test_enableUpgradePath() external {
+        governor.mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1));
+        governor.mapleLoanFactory_registerImplementation(address(factory), 2, address(mapleLoanV2), address(initializerV2));
+
         assertTrue(
-            !notGovernor.try_mapleLoanFactory_disableUpgradePath(address(loanFactory), 1, 2),
-            "Should fail to disable upgrade path: Unauthorized"
+            !governor.try_mapleLoanFactory_enableUpgradePath(address(factory), 1, 1, address(444444)),
+            "Should fail to overwrite initializer"
         );
 
-        assertTrue(governor.try_mapleLoanFactory_enableUpgradePath(address(loanFactory),  1, 2, migrator), "Should not fail to upgrade path");
+        assertTrue(
+            !governor.try_mapleLoanFactory_enableUpgradePath(address(factory), 2, 1, address(444444)),
+            "Should fail to enable a downgrade"
+        );
 
-        assertEq(loanFactory.migratorForPath(1, 2), migrator, "Incorrect state of migratorForPath");
+        assertTrue(
+            !notGovernor.try_mapleLoanFactory_enableUpgradePath(address(factory), 1, 2, address(444444)),
+            "Should fail to enable an upgrade if not governor"
+        );
 
-        assertTrue(governor.try_mapleLoanFactory_disableUpgradePath(address(loanFactory), 1, 2), "Should not fail to upgrade path");
+        assertTrue(
+            governor.try_mapleLoanFactory_enableUpgradePath(address(factory), 1, 2, address(444444)),
+            "Should be able to enable an upgrade path"
+        );
 
-        assertEq(loanFactory.migratorForPath(1, 2), address(0), "Incorrect state of migratorForPath");
+        assertEq(factory.migratorForPath(1, 2), address(444444), "Incorrect migrator");
+
+        assertTrue(
+            governor.try_mapleLoanFactory_enableUpgradePath(address(factory), 1, 2, address(888888)),
+            "Should be able to enable an upgrade path and change the migrator"
+        );
+
+        assertEq(factory.migratorForPath(1, 2), address(888888), "Incorrect migrator");
     }
 
-    function test_upgradeLoan() public {
-        // TODO
+    function test_disableUpgradePath() external {
+        governor.mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1));
+        governor.mapleLoanFactory_registerImplementation(address(factory), 2, address(mapleLoanV2), address(initializerV2));
+        governor.mapleLoanFactory_enableUpgradePath(address(factory), 1, 2, address(444444));
+
+        assertEq(factory.migratorForPath(1, 2), address(444444), "Incorrect migrator");
+
+        assertTrue(
+            !notGovernor.try_mapleLoanFactory_disableUpgradePath(address(factory), 1, 2),
+            "Should fail to disable upgrade path if not governor"
+        );
+
+        assertTrue(
+            governor.try_mapleLoanFactory_disableUpgradePath(address(factory), 1, 2),
+            "Should be able to disable upgrade path"
+        );
+
+        assertTrue(
+            !governor.try_mapleLoanFactory_disableUpgradePath(address(factory), 1, 1),
+            "Should fail to overwrite initializer"
+        );
+
+        assertEq(factory.migratorForPath(1, 2), address(0), "Incorrect migrator");
+    }
+
+    function test_upgradeLoan() external {
+        governor.mapleLoanFactory_registerImplementation(address(factory), 1, address(mapleLoanV1), address(initializerV1));
+        governor.mapleLoanFactory_registerImplementation(address(factory), 2, address(mapleLoanV2), address(initializerV2));
+        governor.mapleLoanFactory_setDefaultVersion(address(factory), 1);
+
+        address[2] memory assets = [address(4567), address(9876)];
+
+        uint256[6] memory parameters = [
+            uint256(0),
+            uint256(10 days),
+            uint256(120_000),
+            uint256(100_000),
+            uint256(365 days / 6),
+            uint256(6)
+        ];
+
+        uint256[2] memory requests = [uint256(300_000), uint256(1_000_000)];
+
+        bytes memory arguments = initializerV1.encodeArguments(address(borrower), assets, parameters, requests);
+
+        MapleLoan loan = MapleLoan(borrower.mapleLoanFactory_createLoan(address(factory), arguments));
+
+        assertEq(loan.implementation(),                    address(mapleLoanV1));
+        assertEq(factory.versionOf(loan.implementation()), 1);
+
+        assertTrue(!borrower.try_loan_upgrade(address(loan), 2, new bytes(0)), "Should not be able to upgrade loan if upgrade path not enabled");
+
+        governor.mapleLoanFactory_enableUpgradePath(address(factory), 1, 2, address(0));
+
+        assertTrue(!notBorrower.try_loan_upgrade(address(loan), 2, new bytes(0)), "Should not be able to upgrade loan if not borrower");
+        assertTrue(   !borrower.try_loan_upgrade(address(loan), 0, new bytes(0)), "Should not be able to upgrade loan to invalid version");
+        assertTrue(   !borrower.try_loan_upgrade(address(loan), 1, new bytes(0)), "Should not be able to upgrade loan to same version");
+        assertTrue(   !borrower.try_loan_upgrade(address(loan), 3, new bytes(0)), "Should not be able to upgrade loan to non-existent version");
+        assertTrue(    borrower.try_loan_upgrade(address(loan), 2, new bytes(0)), "Should be able to upgrade loan");
+
+        assertEq(loan.implementation(),                    address(mapleLoanV2));
+        assertEq(factory.versionOf(loan.implementation()), 2);
     }
 
 }
--- contracts/test/accounts/Governor.sol
+++ contracts/test/accounts/Governor.sol
@@ -9,58 +9,57 @@ contract Governor {
     /*** Direct Functions ***/
     /************************/
 
+    function mapleLoanFactory_disableUpgradePath(address factory_, uint256 fromVersion_, uint256 toVersion_) external {
+        IMapleLoanFactory(factory_).disableUpgradePath(fromVersion_, toVersion_);
+    }
+
+    function mapleLoanFactory_enableUpgradePath(address factory_, uint256 fromVersion_, uint256 toVersion_, address migrator_) external {
+        IMapleLoanFactory(factory_).enableUpgradePath(fromVersion_, toVersion_, migrator_);
+    }
+
     function mapleLoanFactory_registerImplementation(
-        address mapleLoanFactory_,
+        address factory_,
         uint256 version_,
         address implementationAddress_,
         address initializer_
     ) external {
-        IMapleLoanFactory(mapleLoanFactory_).registerImplementation(version_, implementationAddress_, initializer_);
+        IMapleLoanFactory(factory_).registerImplementation(version_, implementationAddress_, initializer_);
     }
 
-    function mapleLoanFactory_enableUpgradePath(address mapleLoanFactory_, uint256 fromVersion_, uint256 toVersion_, address migrator_) external {
-        IMapleLoanFactory(mapleLoanFactory_).enableUpgradePath(fromVersion_, toVersion_, migrator_);
+    function mapleLoanFactory_setDefaultVersion(address factory_, uint256 version_) external {
+        IMapleLoanFactory(factory_).setDefaultVersion(version_);
     }
-
-    function mapleLoanFactory_disableUpgradePath(address mapleLoanFactory_, uint256 fromVersion_, uint256 toVersion_) external {
-        IMapleLoanFactory(mapleLoanFactory_).disableUpgradePath(fromVersion_, toVersion_);
-    }
-
 
     /*********************/
     /*** Try Functions ***/
     /*********************/
 
-    function try_mapleLoanFactory_registerImplementation(
-        address mapleLoanFactory_,
-        uint256 version_,
-        address implementationAddress_,
-        address initializer_
-    ) external returns (bool ok_) {
-        ( ok_, ) = mapleLoanFactory_.call(
-            abi.encodeWithSelector(IMapleLoanFactory.registerImplementation.selector, version_, implementationAddress_, initializer_)
-        );
+    function try_mapleLoanFactory_disableUpgradePath(address factory_, uint256 fromVersion_,uint256 toVersion_) external returns (bool ok_) {
+        ( ok_, ) = factory_.call(abi.encodeWithSelector(IMapleLoanFactory.disableUpgradePath.selector, fromVersion_, toVersion_));
     }
 
     function try_mapleLoanFactory_enableUpgradePath(
-        address mapleLoanFactory_,
+        address factory_,
         uint256 fromVersion_,
         uint256 toVersion_,
         address migrator_
     ) external returns (bool ok_) {
-        ( ok_, ) = mapleLoanFactory_.call(
-            abi.encodeWithSelector(IMapleLoanFactory.enableUpgradePath.selector, fromVersion_, toVersion_, migrator_)
+        ( ok_, ) = factory_.call(abi.encodeWithSelector(IMapleLoanFactory.enableUpgradePath.selector, fromVersion_, toVersion_, migrator_));
+    }
+
+    function try_mapleLoanFactory_registerImplementation(
+        address factory_,
+        uint256 version_,
+        address implementationAddress_,
+        address initializer_
+    ) external returns (bool ok_) {
+        ( ok_, ) = factory_.call(
+            abi.encodeWithSelector(IMapleLoanFactory.registerImplementation.selector, version_, implementationAddress_, initializer_)
         );
     }
 
-    function try_mapleLoanFactory_disableUpgradePath(
-        address mapleLoanFactory_,
-        uint256 fromVersion_,
-        uint256 toVersion_
-    ) external returns (bool ok_) {
-        ( ok_, ) = mapleLoanFactory_.call(
-            abi.encodeWithSelector(IMapleLoanFactory.disableUpgradePath.selector, fromVersion_, toVersion_)
-        );
+    function try_mapleLoanFactory_setDefaultVersion(address factory_, uint256 version_) external returns (bool ok_) {
+        ( ok_, ) = factory_.call(abi.encodeWithSelector(IMapleLoanFactory.setDefaultVersion.selector, version_));
     }
-    
+
 }
